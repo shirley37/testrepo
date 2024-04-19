dc2bc3e26e031fba4e0ee9892d15f144d988859c
shirley37 <167352606+shirley37@users.noreply.github.com>
2024-04-19 11:16:49 -0400

    Create merge_table.r

---
+++ merge_table.r
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+# Function using tidyverse to merge two data frames
+merge_tables_tidyverse <- function(data1, data2, by_column) {
+  merged_data <- inner_join(data1, data2, by = by_column)
+  return(merged_data)
+}
